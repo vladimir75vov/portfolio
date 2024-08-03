c3d3395bcf3dde08a449b1bea4262ec7c03e3a2f
vladimir75vov <vladimir75vov@gmail.com>
2024-08-04 01:34:52 +0300

    [dev] - redirect

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,37 +1,40 @@
 const express = require('express');
 
-const app = express();
-
 const host = '127.0.0.1';
 const port = 7000;
+const defRouter = '/api/v1/';
 
-app.use('/resources/images',express.static('resources/images'));
-app.use('/resources/files',express.static('resources/files'));
+const app = express();
+const productRouter = express.Router();
 
-app.get('/home', (req, res) => {
-    res.status(200).type('text/plain');
-    res.send('Home page')
+productRouter.get('/home', (req, res) => {
+    res.status(200).type('json');
+    res.send(req.query.aaa)
 });
 
-app.get('/about', (req, res) => {
+productRouter.get('/about', (req, res) => {
     res.status(200).type('text/plain');
     res.send('About page');
 });
 
-app.post('/api/admin', (req, res) => {
+productRouter.post('/admin', (req, res) => {
     res.status(200).type('text/plain');
     res.send('Create admin request');
 });
 
-app.post('/api/user', (req, res) => {
+productRouter.post('/api/user', (req, res) => {
     res.status(200).type('text/plain');
     res.send('Create user request');
 });
 
-app.use((req, res) => {
+productRouter.use((req, res) => {
     res.status(404).type('text/plain');
     res.send('Not found');
 });
+
+app.use(defRouter, productRouter);
+app.use('/resources/images',express.static('resources/images'));
+app.use('/resources/files',express.static('resources/files'));
 
 app.listen(port, host, function () {
     console.log(`Server listens http://${host}:${port}`);
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/api/v1/:path*',
+                destination: 'http://127.0.0.1:7000/api/v1/:path*',
+                permanent: true,
+            }
+        ]
+    }
+};
 
 export default nextConfig;
